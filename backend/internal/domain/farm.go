c38397659c6215e1e3f3f27597c9778961fb7115
Natthapol SERMSARAN <natthapolwork@gmail.com>
2025-02-13 22:09:58 +0700

    feat: add domain farm

---
+++ backend/internal/domain/farm.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type Farm struct {
+	UUID      string
+	Name      string
+	Lat       float64
+	Lon       float64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	OwnerID   string
+}
+
+func (f *Farm) Validate() error {
+	return validation.ValidateStruct(f,
+		validation.Field(&f.Name, validation.Required),
+		validation.Field(&f.Lat, validation.Required),
+		validation.Field(&f.Lon, validation.Required),
+		validation.Field(&f.OwnerID, validation.Required),
+	)
+}
+
+type FarmRepository interface {
+	GetByID(context.Context, string) (Farm, error)
+	CreateOrUpdate(context.Context, *Farm) error
+  // Delete(context.Context, string) error
+}
